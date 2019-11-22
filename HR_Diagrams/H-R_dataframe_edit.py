8580cfa027b66f3c1d96487e15a70d68aa266534
Eli Prater <prater0014@gmail.com>
2019-11-22 13:48:06 -0500

    Add text boxes to edit data

---
+++ HR_Diagrams/H-R_dataframe_edit.py
@@ -0,0 +1,10 @@
+from ipywidgets import widgets
+from IPython.display import display
+
+class interactive_df_editor():
+	def __init__(self, df):
+		dataFrame = df
+	
+	def make_inputs(self, editables):
+		for edit in editables:
+			pass
